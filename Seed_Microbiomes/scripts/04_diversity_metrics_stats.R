91e41959618ea2b591745734d9f545a204671d26
amertin-sudo <amertin@student.unimelb.edu.au>
2026-05-19 16:42:15 +1000

    Now runs both rarefied and non rarefied datasets

--- Seed_Microbiomes/scripts/04_diversity_metrics_stats.R
+++ Seed_Microbiomes/scripts/04_diversity_metrics_stats.R
@@ -9,29 +9,29 @@ library(ggplot2)
 #R scripts to get divMeta file
 
 #Observed ASVs_ANOVA
-Obs <- lm(Observed ~ Source*SiteType, data = divMeta, na.action = na.omit)
+Obs <- lm(Observed ~ Source*SiteType, data = divMeta_nonrare, na.action = na.omit)
 summary(Obs)
 anova(Obs)
 #Pairwise tests
 pairs(emmeans(Obs, "SiteType", by = "Source"))
-pairs(emmeans(Obs, "SiteType")) # run this for when only one factor significant
+#pairs(emmeans(Obs, "Source")) # run this for when only one factor significant
+
 #Simpsons_ANOVA
-Sim <- lm(Simpson ~ Source*SiteType, data = divMeta, na.action = na.omit)
+Sim <- lm(Simpson ~ Source*SiteType, data = divMeta_nonrare, na.action = na.omit)
 summary(Sim)
 anova(Sim)
-
 #Pairwise tests
 pairs(emmeans(Sim, "SiteType", by = "Source")) #run this for when interaction term significant
-pairs(emmeans(Sim, "SiteType")) # run this for when only one factor significant
+#pairs(emmeans(Sim, "Source")) # run this for when only one factor significant
 
 #Shannon_ANOVA
-Shan <- lm(Shannon ~ Source*SiteType, data = divMeta, na.action = na.omit)
+Shan <- lm(Shannon ~ Source*SiteType, data = divMeta_nonrare, na.action = na.omit)
 summary(Shan)
 anova(Shan)
 
 #Pairwise tests
 pairs(emmeans(Shan, "SiteType", by = "Source"))
-pairs(emmeans(Shan, "SiteType"))
+#pairs(emmeans(Shan, "Source"))
 
 rm(Obs)
 rm(Shan)
