ce1e09093ffb0bad063786e2447bd8af65ce3004
amertin-sudo <amertin@student.unimelb.edu.au>
2026-05-19 16:42:58 +1000

    Add files via upload

--- Seed_Microbiomes/scripts/05_PCoA.R
+++ Seed_Microbiomes/scripts/05_PCoA.R
@@ -1,9 +1,12 @@
 library(cowplot)
 #Plot PCoA results using Bray Curtis dissimilarities and Hellinger transformed ASV abundance matrices from non-rarefied phyloseq object
 #N.B. Data is subset for each host species##
-composition <- transform(bacteria_subset, transform = "Hellinger")
-meta <- meta(composition)
+#Load in phyloseq objects for each host#
 
+# Load Melaleuca .rds file
+bacteria_MQ <- readRDS("MQ_phyloseq_16S.rds")
+composition <- transform(bacteria_MQ, transform = "log")
+meta <- meta(composition)
 
 #this script has 95% confidence ellipses around data points from each grouping 
 
@@ -15,11 +18,48 @@ A <- plot_ordination(physeq = composition,
                 shape = "SiteType") + 
   theme_bw() + theme(legend.position = "right") +
   stat_ellipse(level = 0.95) +
-  geom_point(size=1.5)+ ggtitle("Bray")
+  geom_point(size=1.5)+ ggtitle("Melaleuca quinquenervia - Bacteria")
 print(A)
 
-#Run analysis for each host species, separately subset and each for bacteria and fungi and combine using cowplot grid
+
+# Load Microlaena .rds file
+bacteria_MS <- readRDS("MS_phyloseq_16S.rds")
+composition <- transform(bacteria_MS, transform = "log")
+meta <- meta(composition)
+
+#this script has 95% confidence ellipses around data points from each grouping 
+
+bray <- ordinate(composition, method = "PCoA", distance = "bray")
+B <- plot_ordination(physeq = composition,
+                     ordination = bray,
+                     type = "samples",
+                     color = "SiteType",
+                     shape = "SiteType") + 
+  theme_bw() + theme(legend.position = "right") +
+  stat_ellipse(level = 0.95) +
+  geom_point(size=1.5)+ ggtitle("Microlaeana stipoides - Bacteria")
+print(B)
+
+# Load Themeda .rds file
+bacteria_TT <- readRDS("TT_phyloseq_16S.rds")
+composition <- transform(bacteria_TT, transform = "log")
+meta <- meta(composition)
+
+#this script has 95% confidence ellipses around data points from each grouping 
+
+bray <- ordinate(composition, method = "PCoA", distance = "bray")
+C <- plot_ordination(physeq = composition,
+                     ordination = bray,
+                     type = "samples",
+                     color = "SiteType",
+                     shape = "SiteType") + 
+  theme_bw() + theme(legend.position = "right") +
+  stat_ellipse(level = 0.95) +
+  geom_point(size=1.5)+ ggtitle("Themeda triandra - Bacteria")
+print(C)
+
+#Run analysis for each host species, separately subset and each for bacteria and fungi and combine
 #Construct PCoA combined Figure 3 ##
-cowplot::plot_grid(A, B, C, D, E, F, nrow = 3)
+cowplot::plot_grid(A, B, C, nrow = 3)
 
 #----------------------------------------------#
