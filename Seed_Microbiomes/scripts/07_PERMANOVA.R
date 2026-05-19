39611f77983a8690477ee2c76d1f58942c0a4442
amertin-sudo <amertin@student.unimelb.edu.au>
2026-05-19 16:44:48 +1000

    Added beta disp for fixed factors

--- Seed_Microbiomes/scripts/07_PERMANOVA.R
+++ Seed_Microbiomes/scripts/07_PERMANOVA.R
@@ -2,26 +2,49 @@
 ##     PERMANOVA   ##
 ##############################
 
-## A PERMANOVA was run on the Hellinger transformed data set with a Bray Curtis distance matrix
+## A PERMANOVA was run on the log transformed data set with a Bray Curtis distance matrix
 library(vegan)
 library(phyloseq)
 library(microbiome)
 library(EcolUtils)
 
 #Compositional (Hellinger) transform abundance data
-compositional <- microbiome::transform(bacteria, transform = "Hellinger")
+compositional <- microbiome::transform(bacteria, transform = "log")
 
 #Generate Bray-Curtis distance matrix
 bray_dist_matrix <- phyloseq::distance(compositional, method = "bray") 
 
 #Dispersion test and plot; homogeneity of dispersion among groups is an assumption for adonis
 
-#Bray Dispoersion tests -Betadisp
-dispr.bray <- vegan::betadisper(bray_dist_matrix, phyloseq::sample_data(compositional)$Source_SiteType)
-dispr.bray
-plot(dispr.bray)
-anova(dispr.bray) 
+#Bray Dispersion tests -Betadisp
 #reject the assumption of homogeneity of dispersion by type if p < 0.05
+
+# Extract metadata
+meta <- as(sample_data(compositional), "data.frame")
+
+meta$Source <- as.factor(meta$Source)
+meta$SiteType <- as.factor(meta$SiteType)
+meta$Source_SiteType <- interaction(meta$Source, meta$SiteType)
+
+####################################
+# Dispersion tests
+####################################
+
+# Source
+disp_source <- betadisper(bray_dist_matrix, meta$Source)
+anova(disp_source)
+permutest(disp_source, permutations = 999)
+
+# SiteType
+disp_sitetype <- betadisper(bray_dist_matrix, meta$SiteType)
+anova(disp_sitetype)
+permutest(disp_sitetype, permutations = 999)
+
+# Interaction grouping
+disp_interaction <- betadisper(bray_dist_matrix, meta$Source_SiteType)
+anova(disp_interaction)
+permutest(disp_interaction, permutations = 999)
+
 
 # Run PerMANOVA with stratified/constrained permutations to account for non-independence between site pairs.
 
@@ -45,6 +68,6 @@ adonis2(
 
 
 # Use function by Pedro Martine Zarbizu https://github.com/pmartinezarbizu/pairwiseAdonis
-pairwiseAdonis::pairwise.adonis2(bray_dist_matrix ~ Source*SiteType, data = meta, strata = meta$SitePair)
+pairwiseAdonis::pairwise.adonis2(bray_dist_matrix ~ Host_SiteType, data = meta)
 
 ####-----------------------------------------------------------------###
